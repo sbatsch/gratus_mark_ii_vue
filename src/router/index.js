59095a5abb74f174acd5a8b3317aae4cf8713b56
sbatsch <sbatsch2@gmail.com>
2020-09-22 19:03:36 -0500

    signup component added

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,17 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import Home from '../views/Home.vue';
+import About from '../views/About.vue';
+import Test from '../views/Test.vue';
+import Signup from '../views/Signup.vue';
 
 Vue.use(VueRouter)
 
-  const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+const routes = [
+  { path: '/', name: 'home', component: Home },
+  { path: '/about', name: 'about', component: About },
+  { path: '/test', name: 'test', component: Test },
+  { path: '/signup', name: 'signup', component: Signup },
 ]
 
 const router = new VueRouter({
---
+++ src/views/Test.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="test">
+    <h1>The Test Page</h1>
+  </div>
+</template>
+
+<style>
+</style>
+
+<script>
+export default {
+  data: function () {
+    return {
+      message: "Welcome to Vue.js!",
+    };
+  },
+  created: function () {},
+  methods: {},
+};
+</script>
